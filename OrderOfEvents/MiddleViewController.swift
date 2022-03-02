818ee4b0ef48a7e3fa8c5d951f4c97e86867fde9
marie <dul.sreyneang@gmail.com>
2022-03-02 14:53:09 +0800

    LifeCycle
    Add five life-cycle methods to called each states.

--- OrderOfEvents/MiddleViewController.swift
+++ OrderOfEvents/MiddleViewController.swift
@@ -9,21 +9,19 @@ import UIKit
 
 class MiddleViewController: UIViewController {
 
+    @IBOutlet var label: UILabel!
+    
+    var eventNumber = 1
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        addEvent(from: "viewDidLoad")
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func addEvent(from: String) {
+        if let existingText = label.text {
+            label.text = "\(existingText)\nEvent number \(eventNumber) was \(from)"
+            eventNumber += 1
+        }
     }
-    */
-
 }
